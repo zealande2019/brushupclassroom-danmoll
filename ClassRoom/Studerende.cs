e5322f262cf9773380547c056cde588c294c6f43
DanMoll <dani948d@edu.easj.dk>
2019-02-06 21:20:44 +0100

    Tilføjede Studerende klasse. Properties og constructor.

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        string Name;
+        int Birthmonth;
+        int Birthday;
+
+        public Studerende(string navn, int bmonth, int bday)
+        {
+            Name = navn;
+            Birthmonth = bmonth;
+            Birthday = bday;
+        }
+    }
+}
