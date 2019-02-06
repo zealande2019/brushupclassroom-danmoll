54be8b18abfcf7effd32884694e9ec0437ccec98
DanMoll <dani948d@edu.easj.dk>
2019-02-06 21:29:28 +0100

    Tilføjede klassen KlasseRum. Properties, Constructor.

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        string KlasseNavn;
+        List<Studerende> Klasseliste;
+        DateTime SemesterStart;
+
+        public KlasseRum(string navn, List<Studerende> elev, DateTime semStart)
+        {
+
+        }
+    }
+}
