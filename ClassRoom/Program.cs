4aed439fc38e0728a6999779b3f571199368a005
DanMoll <dani948d@edu.easj.dk>
2019-02-07 09:26:02 +0100

    Indsatte nogle kommentarer.

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,20 +10,27 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            //Opretter instans af Klasserum
             KlasseRum klasseRum = new KlasseRum();
             klasseRum.KlasseNavn = "3B";
 
+            //Indsættelse af Studerende i listen KlasseListe
             Studerende elev1 = new Studerende("Daniel Møller", 11, 28);
             Studerende elev2 = new Studerende("Louise Møller", 05, 04);
             klasseRum.KlasseListe.Add(elev1);
             klasseRum.KlasseListe.Add(elev2);
 
+
             Console.WriteLine("Klasse: " + klasseRum.KlasseNavn.ToString());
             Console.WriteLine("Indeholder: ");
+
+            //Lægger KlasseListe i en foreach for at kunne pille data ud af de indlagte Studerende.
             foreach (var item in klasseRum.KlasseListe)
             {
                 Console.WriteLine("Navn: " + item.Name + ", har fødselsdato d. " + item.BDay + ". i " + item.BMonth + "." );
             }
+
+            //Stopper den fra at bare at fræse igennem.
             Console.ReadLine();
         }
     }
